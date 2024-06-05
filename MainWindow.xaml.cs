a246d3d7e32095cfb90e5b49a7bed05d6b668c5f
Ilya Fadeev <109843076+weirdddlich@users.noreply.github.com>
2024-06-05 10:16:16 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PasswordGenerator.Extensions;
+
+namespace PasswordGenerator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnGenerate_OnClick(object sender, RoutedEventArgs e)
+    {
+        PasswordGeneratoros generator = new PasswordGeneratoros();
+
+        switch (CbDiff.SelectedIndex)
+        {
+            case 0:
+                TbBox.Text = generator.GeneratePassword(DfPass.Easy, Convert.ToInt32(Tblenght.Text));
+                break;
+            case 1:
+                TbBox.Text = generator.GeneratePassword(DfPass.Medium, Convert.ToInt32(Tblenght.Text));
+                break; 
+            case 2:
+                TbBox.Text = generator.GeneratePassword(DfPass.Hard, Convert.ToInt32(Tblenght.Text));
+                break; 
+        }
+    }
+}
